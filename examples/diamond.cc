b60873ced31f2878c91905727db46fce738cc764
Axel Huebl <axel.huebl@web.de>
2014-01-09 17:18:27 +0100

    Fix FreeType Add Diamond Example
    - adds Debian Bug report #633405 example (thx Kevin Ryde)
    - CMake fix if FreeType was not found

---
+++ examples/diamond.cc
@@ -0,0 +1,19 @@
+/** Example and bug report from Kevin Ryde
+  *
+  * http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=633405
+  */
+
+#include <pngwriter.h>
+#include <cstdlib>
+
+int main( )
+{
+  pngwriter img( 40,11, 0, "/tmp/foo.png" );
+
+  img.diamond      ( 10,6, 8,8,  1.0,1.0,1.0 );
+  img.filleddiamond( 30,6, 8,8,  1.0,1.0,1.0 );
+  img.write_png();
+
+  system( "convert /tmp/foo.png /tmp/foo.xpm; cat /tmp/foo.xpm" );
+  return 0;
+}
